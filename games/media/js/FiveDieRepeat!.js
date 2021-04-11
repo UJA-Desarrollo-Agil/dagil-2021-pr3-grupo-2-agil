9b2f80e1e5a5320a58280646a323519362fd5567
Esteban Jodar Pozo <ejp00003@red.ujaen.es>
2021-04-11 22:55:59 +0200

    Pantalla de inicio a nuestra Historia de Usuario

---
+++ games/media/js/FiveDieRepeat!.js
@@ -0,0 +1,89 @@
+/* IDENTIFICADOR UNICO. Principalmente se usa para los juegos largos
+ * en los que tienes que guardar la partida, para que se sepa en todo momento
+ * quien y por donde va la trama del juego. */
+undum.game.id = "be3d95b6-cbc7-48c6-8e6c-49837ba9113e";
+
+/* VERSION DEL JUEGO. Aqui se controlan las partidas guardadas. Evidentemente,
+ * una partida guardada, al cambiar la version del juego, si luego la cargas
+ * no va a funcionar. */
+undum.game.version = "2.0";
+
+/* Variable usada para Web responsive. */
+undum.game.mobileHide = 2000;
+
+/* Variable la opcion de velocidad del fade out. */
+undum.game.fadeSpeed = 1500;
+
+/* Variable que cambia la velocidad de deslizamiento al pulsar una opcion. */
+undum.game.slideUpSpeed = 500;
+
+
+
+/* SITUACIONES DEL JUEGO. CADA UNA CON UN UNICO ID. */
+undum.game.situations = {
+    inicio: new undum.SimpleSituation(            
+            
+        "<H1><a href='nuevojuego' class='click'><span id='span1'></span><span id='span2'></span><span id='span3'></span> \
+                <span id='span4'></span><center>NUEVO JUEGO</center></a></H1> \
+        <H1><a href='cargarjuego' class='click'><span id='span1'></span><span id='span2'></span><span id='span3'></span> \
+                <span id='span4'></span><center>CARGAR JUEGO</center></a></H1> \
+        <H1><a href='salir' class='click'><span id='span1'></span><span id='span2'></span><span id='span3'></span> \
+                <span id='span4'></span><center>SALIR</center></p></H1>"
+            ),
+    nuevojuego: new undum.SimpleSituation(
+            "<h1>Aqui iniciariamos un nuevo juego preguntandole al usuario su nombre.</h1>"
+            ),
+    cargarjuego: new undum.SimpleSituation(
+            "<h1>Aqui cargariamos un juego que previamente se haya guardado.</h1>"
+    ),
+    salir: new undum.SimpleSituation(
+            "<h1>Y aqui se volveria a la pantalla de inicio.</h1>"
+            
+            
+    )
+};
+
+// ---------------------------------------------------------------------------
+/* SITUACION DE INICIO DEL JUEGO. */
+undum.game.start = "inicio";
+
+
+// ---------------------------------------------------------------------------
+/*AQUI SE DEFINEN LAS CUALIDADES DEL PERSONAJE. */
+undum.game.qualities = {
+    interes: new undum.IntegerQuality(
+            "Interés", {priority: "0001", group: 'status'}
+    ),
+    conocimiento: new undum.IntegerQuality(
+            "Conocimiento", {priority: "0001", group: 'status'}
+    ),
+    pasta: new undum.IntegerQuality(
+            "Pasta", {priority: "0003", group: 'inventario'}
+    ),
+    suspenso: new undum.OnOffQuality(
+            "Suspenso", {priority: "0001", group: 'progreso', onDisplay: "&#10003;"}
+    ),
+    aprobado: new undum.OnOffQuality(
+            "Aprobado", {priority: "0002", group: 'progreso', onDisplay: "&#10003;"}
+    )
+};
+
+// ---------------------------------------------------------------------------
+/* GRUPOS DE CUALIDADES. */
+undum.game.qualityGroups = {
+    status: new undum.QualityGroup('Habilidades', {priority: "0001"}),
+    inventario: new undum.QualityGroup('Inventario', {priority: "0002"}),
+    progreso: new undum.QualityGroup('Progreso', {priority: "0003"})
+};
+
+// ---------------------------------------------------------------------------
+/* This function gets run before the game begins. It is normally used
+ * to configure the character at the start of play. */
+undum.game.init = function (character, system) {
+    character.qualities.interes = 50;
+    character.qualities.conocimiento = 0;
+    character.qualities.pasta = 200;
+    character.qualities.suspenso = 1;
+    character.qualities.aprobado = 0;
+    system.setCharacterText("<p>Estado del personaje:</p>");
+};
