5039b65e0bf3b010b49c527c1994f68feb0a1bde
Esteban Jodar Pozo <ejp00003@red.ujaen.es>
2021-05-02 14:33:17 +0200

    Citando al jugador por su nombre y preparado el sistema de usuario y contraseña ademas del boton Salir que refresca la pagina y la vuelve al principio

--- games/media/js/FiveDieRepeat!.js
+++ games/media/js/FiveDieRepeat!.js
@@ -1,3 +1,17 @@
+var nombreUsuario;
+var claveUsuario;
+
+function ir() {
+    window.alert('Para una experiencia mas personalizada, este juego registra usuario y contraseña.\n\
+                     Si aun no lo ha hecho, solo le llevara unos segundos...');
+}
+function ir2() {
+    nombreUsuario = document.getElementById('nombre').value;
+    claveUsuario = document.getElementById('password').value;
+    window.alert('Bienvenido ' + nombreUsuario + ', su nombre y clave han quedado registrados. Acepte y pulse ADELANTE para continuar.');
+    return nombreUsuario;
+}
+
 /* IDENTIFICADOR UNICO. Principalmente se usa para los juegos largos
  * en los que tienes que guardar la partida, para que se sepa en todo momento
  * quien y por donde va la trama del juego. */
@@ -22,7 +36,7 @@ undum.game.slideUpSpeed = 500;
 /* SITUACIONES DEL JUEGO. CADA UNA CON UN UNICO ID. */
 undum.game.situations = {
     inicio: new undum.SimpleSituation(
-            "<H1><a href='nuevojuego' class='click'><span id='span1'></span><span id='span2'></span><span id='span3'></span> \
+            "<H1><a href='nuevojuego' class='click' onclick='ir()'><span id='span1'></span><span id='span2'></span><span id='span3'></span> \
                 <span id='span4'></span><center>NUEVO JUEGO</center></a></H1> \
         <H1><a href='cargarjuego' class='click'><span id='span1'></span><span id='span2'></span><span id='span3'></span> \
                 <span id='span4'></span><center>CARGAR JUEGO</center></a></H1> \
@@ -30,20 +44,41 @@ undum.game.situations = {
                 <span id='span4'></span><center>SALIR</center></p></H1>"
             ),
     nuevojuego: new undum.SimpleSituation(
-            "<h1>Nombre del personaje?</h1>",
-            ),
-    cargarjuego: new undum.SimpleSituation(
-            "<h1>Aqui cargariamos un juego que previamente se haya guardado.</h1>"
+            "<form name='formulario' action=''>\
+            <h3><div><label for='nombre'>Nombre</label><input type='text' name='nom' id='nombre' value= '' class='marco'></label></div></h3>\
+            <h3><div><label for='password'>Contraseña</label><input type='password' name='pass' id='password' value= '' class='marco'></label></div></h3>\
+            <center><input type='button' value='ACEPTAR' onclick='ir2()' VSPACE='40' class='marco'>\
+            </FORM></center>\
+            <center><H1><a href='bienvenida' class='click'><span id='span1'></span><span id='span2'></span><span id='span3'></span> \
+                <span id='span4'></span>Adelante</a></H1>",
+            {
+                heading: "_____Registro_____"
+            }
+    ),
+    bienvenida: new undum.SimpleSituation(
+            function ir3() {
+                const usu = document.getElementById('nombre').value;
+                const pass = document.getElementById('password').value;
+                return "<p><h1>Bienvenido " + usu + ", que sepas que tu clave es: " + pass + ", disfruta del juego.</h1></p>\
+                        <p>Y pensaba " + usu + "...<a href='nueva' class='marco'>podre cambiar de pagina?</a> estando el\
+                        hiperenlace dentro del return de una funcion??</p>";
+            }
+    ),
+    nueva: new undum.SimpleSituation(
+            "<p><h1>Pues si!!! Jajajajaja</h1></p>\
+             <p>Pero la putada es que cada vez que tengamos que hacer referencia al usu, vamos a tener que meter lo que vayamos\
+             a poner de situation dentro del return de una funcion como la go3() xk el scope de la variable es dentro de la funcion\n\
+             y no he encontrado la manera de sacarla fuera. Ah, los enlaces con class='marco' estan muy chulos ya estan en el CSS.</p>"
+            
+
             ),
     salir: new undum.SimpleSituation(
-            "<h1>Y aqui se volveria a la pantalla de inicio.</h1>",
+            "<h1>Inicio.</h1>",
             {
                 enter: function () {
                     location.reload();
                 }
             }
-
-
     )
 };
 
